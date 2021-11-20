a0455840f4e9691922dee9d4c9172aad07aa06e3
Johnexzy <obajohn75@gmail.com>
2021-11-20 23:09:40 +0100

    refactor(run.js): refactoring run.js

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -2,10 +2,33 @@
 
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import 'hardhat/console.sol';
 
-contract MyNFT {
-    constructor() {
+contract MyNFT is ERC721URIStorage {
+    
+
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721 ("OJONFT", "OJO"){
         console.log("My awesome NFT collection");
+    }
+
+    function makeAnNFT() public {
+        uint256 tokenId = _tokenIds.current();
+        
+        // Actually mint the NFT to the sender using msg.sender.
+        _safeMint(msg.sender, tokenId);
+
+        // set the NFT data
+        _setTokenURI(tokenId, "Hello John");
+
+        // incrementing the counter for when NFT is minted
+        _tokenIds.increment();
+
+
     }
 }
